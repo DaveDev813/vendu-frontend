4213dac1c1be09ff1e0ffc676644d436011f9d1d
DaveDev13 <bobadilla.rodave@gmail.com>
2020-01-14 20:41:21 +0800

    add initial design

--- src/components/QrReader.tsx
+++ src/components/QrReader.tsx
@@ -1,5 +1,6 @@
 import React, { Component, ReactElement } from 'react';
 import QrReader from 'react-qr-reader';
+import '../scss/bgWave.scss';
 
 class QrScanner extends Component {
   state = {
@@ -19,13 +20,61 @@ class QrScanner extends Component {
   render(): ReactElement {
     return (
       <div>
-        <QrReader
-          delay={300}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          style={{ width: '300px', height: '300px' }}
-        />
-        <p>{this.state.result}</p>
+        {/* <div className="title">{this.state.result}</div> */}
+
+        <div>
+          <div className="header">
+            <div className="inner-header flex">
+              <QrReader
+                delay={300}
+                onError={this.handleError}
+                onScan={this.handleScan}
+                style={{ width: '300px', height: '300px' }}
+              />
+            </div>
+            <div>
+              <svg
+                className="waves"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                viewBox="0 24 150 28"
+                preserveAspectRatio="none"
+                shapeRendering="auto"
+              >
+                <defs>
+                  <path
+                    id="gentle-wave"
+                    d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+                  />
+                </defs>
+                <g className="parallax">
+                  <use
+                    xlinkHref="#gentle-wave"
+                    x={48}
+                    y={0}
+                    fill="rgba(255,255,255,0.7"
+                  />
+                  <use
+                    xlinkHref="#gentle-wave"
+                    x={48}
+                    y={3}
+                    fill="rgba(255,255,255,0.5)"
+                  />
+                  <use
+                    xlinkHref="#gentle-wave"
+                    x={48}
+                    y={5}
+                    fill="rgba(255,255,255,0.3)"
+                  />
+                  <use xlinkHref="#gentle-wave" x={48} y={7} fill="#fff" />
+                </g>
+              </svg>
+            </div>
+          </div>
+          <div className="content flex">
+            <h1>{this.state.result}</h1>
+          </div>
+        </div>
       </div>
     );
   }
